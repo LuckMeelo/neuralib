2f7e7e20c0cf05f144b882e691f251de9c0a7991
LuckMeelo <charmeel.vodouhe@epitech.eu>
2024-02-26 17:25:52 +0100

    (feat): added sigmoid activation function

--- examples/perceptron_example.py
+++ examples/perceptron_example.py
@@ -15,9 +15,20 @@
 import numpy as np
 import neuralib as ml
 
+# load csv data
 X, y = ml.data.load_csv("./data/AND_dataset.csv", "output")
+
+# perceptron initialization
 perceptron = ml.Perceptron(nb_inputs=2, activation='binary_step')
+
+# perceptron training
 perceptron.train(X_sample=X, y_expected=y)
+
+# perceptron prediction on test data sets
 predicted = perceptron.predict(X)
+
+# output predictions
 print(predicted)
-print((predicted == np. reshape(y, -1)).all())
+
+# test prediction value
+print((predicted == np.reshape(y, -1)).all())
--- neuralib/activation.py
+++ neuralib/activation.py
@@ -13,6 +13,7 @@ import numpy as np
 # ArcTan activation function
 # ReLU (Rectified Linear Unit) Activation Function
 # soft max
+# sigmoid function or Logistic activation function
 
 #  Callable[[np.ndarray], np.ndarray]
 
@@ -32,6 +33,10 @@ def relu(y: np.ndarray, *, derivative=False) -> np.ndarray:
 def softmax(y: np.ndarray, *, derivative=False) -> np.ndarray:
     exp_scores = np.exp(y)
     return exp_scores / np.sum(exp_scores)
+
+
+def sigmoid_function(y: np.ndarray, *, derivative=False) -> np.ndarray:
+    return (1/(1 + np.exp(- y)))
 
 
 # TODO also store derivatives

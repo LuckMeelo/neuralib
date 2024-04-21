40cd7bc7ef45add53989df7de78544434330ed7d
LuckMeelo <charmeel.vodouhe@epitech.eu>
2024-04-21 13:41:41 +0100

    feat: added util function to print array dimensions

---
+++ neuralib/utils/__init__.py
@@ -0,0 +1,8 @@
+##
+# EPITECH PROJECT, 2024
+# neural_test [WSL : Ubuntu-22.04]
+# File description:
+# utils
+##
+
+from .array import *
---
+++ neuralib/utils/array.py
@@ -0,0 +1,18 @@
+##
+# EPITECH PROJECT, 2024
+# neural_test [WSL : Ubuntu-22.04]
+# File description:
+# array
+##
+
+import numpy as np
+
+
+def display_2D_dims(arr: np.ndarray, verbose=False) -> None:
+    if (verbose):
+        print(arr)
+    if (len(arr.shape) not in [1, 2]):
+        raise "Shape error"
+    x = arr.shape[0] if (len(arr.shape) == 1) else arr.shape[1]
+    y = 1 if (len(arr.shape) == 1) else arr.shape[0]
+    return (f"{x}x{y}")
